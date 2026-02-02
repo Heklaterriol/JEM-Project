39127ae4b6a3c8d73891103703355b6b37ae0c60
Hekla Terriol <webstuff@sternenaugen.net>
2026-02-03 00:54:40 +0100

    Update script.php

--- modules/mod_jem_timeline/script.php
+++ modules/mod_jem_timeline/script.php
@@ -3,7 +3,6 @@
  * @package    JEM
  * @subpackage JEM Timeline Module
  * @copyright  (C) 2013-2026 joomlaeventmanager.net
- * @copyright  (C) 2005-2009 Christoph Lukes
  * @license    https://www.gnu.org/licenses/gpl-3.0 GNU/GPL
  */
 
